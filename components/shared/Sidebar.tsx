e4ce56f2555184c5b2243726847f4d81505b4dca
Patrick Kim <patrick.yrkim@gmail.com>
2024-03-04 00:05:48 -0800

    Update link styling

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -21,7 +21,26 @@ const Sidebar = () => {
         <nav className='sidebar-nav'>
           <SignedIn>
             <ul className='sidebar-nav_elements'>
-              {navLinks.map((link) => {
+              {navLinks.slice(0, 6).map((link) => {
+                const isActive = link.route === pathname
+
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+                    <Link className='sidebar-link' href={link.route}>
+                      <Image 
+                        src={link.icon} 
+                        alt="logo" 
+                        width={24} 
+                        height={24} 
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+            <ul className='sidebar-nav_elements'>
+              {navLinks.slice(6).map((link) => {
                 const isActive = link.route === pathname
 
                 return (
