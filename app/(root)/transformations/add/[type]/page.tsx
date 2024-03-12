c877d3e7e3ac9bd1216ab3146b0efe0aa39e3b78
Patrick Kim <patrick.yrkim@gmail.com>
2024-03-11 19:00:46 -0700

    Update dynamic header type

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({ params: { type }}: SearchParamProps) => {
+  const transformation = transformationTypes[type]
+
   return (
     <Header 
-      title="Transformation Title"
-      subtitle="Transformation Subtitle"
+      title={transformation.title}
+      subtitle={transformation.subTitle}
     />
   )
 }
