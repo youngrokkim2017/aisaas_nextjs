2114712d77d5ec3e82d02f6baf331dda0893c88d
Patrick Kim <patrick.yrkim@gmail.com>
2024-03-14 22:52:37 -0700

    Update transformation form

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -15,17 +15,28 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { defaultValues } from "@/constants"
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50)
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
 })
 
-const TransformationForm = () => {
+const TransformationForm = ({ action, data = null }: TransformationFormProps) => {
+  const initialValues = data && action === 'Update' ? {
+    title: data?.title,
+    aspectRatio: data?.aspectRatio,
+    color: data?.color,
+    prompt: data?.prompt,
+    publicId: data?.publicId,
+  } : defaultValues
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: '',
-    }
+    defaultValues: initialValues
   })
 
   function onSubmit(values: z.infer<typeof formSchema>) {
@@ -37,7 +48,7 @@ const TransformationForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="title"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Username</FormLabel>
