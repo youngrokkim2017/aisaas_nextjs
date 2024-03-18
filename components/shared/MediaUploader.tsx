5f46460c807f0bf65bf14b6bdf6f9c079294bca0
Patrick Kim <patrick.yrkim@gmail.com>
2024-03-18 16:32:20 -0700

    Add MediaUploader component

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useToast } from '@/components/ui/use-toast'
+
+const MediaUploader = () => {
+  const { toast } = useToast()
+
+  return (
+    <div>MediaUploader</div>
+  )
+}
+
+export default MediaUploader
