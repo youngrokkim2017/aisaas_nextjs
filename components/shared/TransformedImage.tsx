2084f3c440b7a56975a485075b5fa39fe52a20c2
Patrick Kim <patrick.yrkim@gmail.com>
2024-03-23 13:52:04 -0700

    Update transformed image

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
+import { CldImage } from 'next-cloudinary'
+import { dataUrl, debounce, getImageSize } from '@/lib/utils'
+import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props'
 
 const TransformedImage = ({ 
   image, 
@@ -36,7 +39,35 @@ const TransformedImage = ({
 
       {image?.publicId && transformationConfig ? (
         <div className='relative'>
-          
+          <CldImage 
+            width={getImageSize(type, image, "width")}
+            height={getImageSize(type, image, "height")}
+            src={image?.publicId}
+            alt={image.title}
+            sizes={"(max-width: 767px) 100vw, 50vw"}
+            placeholder={dataUrl as PlaceholderValue}
+            className="transformed-image"
+            onLoad={() => {
+              setIsTransforming && setIsTransforming(false) 
+            }}
+            onError={() => {
+              debounce(() => {
+                setIsTransforming && setIsTransforming(false) 
+              }, 8000)
+            }}
+            {...transformationConfig}
+          />
+
+          {isTransforming && (
+            <div className='transforming-loader'>
+              <Image 
+                src="/assets/icons/spinner.svg"
+                width={50}
+                height={50}
+                alt="Transforming"
+              />
+            </div>
+          )}
         </div>
       ) : (
         <div className='transformed-placeholder'>
