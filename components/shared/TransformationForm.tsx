5c0d920d4f7ef9ce865c74a73f14610cf35716f8
Patrick Kim <patrick.yrkim@gmail.com>
2024-04-01 20:36:50 -0700

    Update credits

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -170,8 +170,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
     setNewTransformation(null)
 
     startTransition(async () => {
-      await updateCredits(userId, -1)
-      // update creditFee
+      await updateCredits(userId, creditFee)
     })
   }
 
