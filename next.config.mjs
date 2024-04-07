4274e4414a249992f5589a0154ef5e52a3f576e1
Patrick Kim <patrick.yrkim@gmail.com>
2024-04-06 22:25:36 -0700

    Fix cloudinary error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        images: {
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'res.cloudinary.com',
+                    port: ''
+                }
+            ]
+        }
+    }
+};
 
 export default nextConfig;
